50e4cf519bbf20b4c7946b6178126b3eb2d4d983
austin <austinkk@sina.cn>
2019-08-05 23:50:57 +0800

    update

--- utils/FE.py
+++ utils/FE.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 import pandas as pd
@@ -7,7 +6,13 @@ import math
 from scipy import stats
 import sklearn.preprocessing as preproc
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
+import nltk
+import jieba
+import jieba.posseg as pseg
+import sklearn.preprocessing.OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction import FeatureHasher
 
 # # 数值特征
 
@@ -57,9 +62,6 @@ def box_cox(x, lambda_ = 0, auto = True):
     else:
         result = stats.boxcox(x, lambda_)
     return result
-
-
-
 
 # 特征缩放，归一化
 # 不要＂中心化＂稀疏数据，如果平移量不是０，会导致多数元素为０的稀疏特征向量变成密集特征向量，会给分类器带来巨大负担
@@ -204,3 +206,128 @@ class TfIdf(object):
             stopword = fp.read().decode('utf-8')  # 提用词提取
         #将停用词表转换为list
         return stopword.splitlines()
+
+
+
+
+# 切词
+# jieba tools
+class JiebaTools(object):
+    def __init__(self):
+        pass
+
+    def get_words(s, cut_all = False, sep = ' '):
+        # cut all为True为全模式，各种词都会匹配出来，不能解决歧义
+        # cut all为False为精确模式，适合文本分析
+        return sep.join(jieba.cut(s, cut_all = cut_all))
+
+    def get_words_search(s, sep = ' '):
+        # 搜索引擎模式
+        return sep.join(jieba.cut_for_search(s))
+
+    def load_userdict(file_name):
+        # 输入格式: 一行一个词，词语、词频(可省略)、词性(可省略)
+        jieba.load_userdict(file_name)
+
+    # 动态修改词典
+    def add_word(word, freq = None, tag = None):
+        jieba.add_word(word, freq, tag)
+
+    def del_word(word):
+        jieba.del_word(word)
+
+    # 调节词频
+    # 拆开两个字 AB -> A B
+    def break_word(A, B):
+        jieba.suggest_freq((A, B), True)
+    # 避免被拆开 A B -> AB
+    def combine(word):
+        jieba.suggest_freq(word, True)
+
+    def get_word_pseg(sent):
+        words = pseg.cut(sent)
+        result = []
+        for word, flag in words:
+            result.append((word, flag))
+        return result
+
+# nltk tools
+class NltkTools(object):
+    def __init__(self):
+        self.stemmer = nltk.stem.porter.PorterStemmer()
+
+    # 词干提取
+    def get_stem(self, word):
+        return self.stemmer.stem(word)
+
+class DiscreteEncoder(object):
+    def __init__(self):
+        self.le = LabelEncoder()
+        self.oh = OneHotEncoder()
+
+    # label encoder
+    def fit_label_encoder(self, data):
+        self.le.fit(data)
+        return self.label_coding(data)
+
+    def label_coding(self, data):
+        return self.le.transform(data)
+
+    def label_decoding(self, data):
+        return self.le.inverse_transform(data)
+
+    def get_label_classes(self):
+        return self.le.classes_
+
+    def fit_onehot_encoder(self, data):
+        self.oh.fit(data)
+        return self.onehot_coding(data)
+
+    def onehot_coding(self, data):
+        return self.oh.transform(data).toarray()
+
+    def virtual_coding(self):
+        # TODO
+        pass
+
+    def effect_coding(self):
+        # TODO
+        pass
+
+# 处理大型分类变量
+
+# 特征散列化 通常用于线性模型
+def hash_features(word_list, m):
+    output = [0] * m
+    for word in word_list:
+        index = hash(word) % m
+        output[index] += 1
+    return output
+
+# 带正负号的特征散列化
+# 确保散列后的特征之间的内积等于初始特征内积的期望
+def hash_features(word_list, m):
+    output = [0] * m
+    for word in word_list:
+        index = hash(word) % m
+        sign_bit = hash(word) % 2
+        if (sign_bit == 0):
+            output[index] -= 1
+        else:
+            output[index] += 1
+    return output
+
+class FeatureHash(object):
+    # string -> m维向量
+    def __init__(self, m):
+        self.fh = FeatureHasher(n_features = m, input_type = 'string')
+        self.hv = HashingVectorizer(n_features = m)
+
+    def fh_transform(self, data):
+        return self.fh.transform(data).toarray()
+    
+    def hv_transform(self, data):
+        return self.hv.transform(data).toarray()
+
+# 分箱计数
+# 用户在某广告的的优势比 用户 ctr / 此广告其他用户的ctr(可以取log)
